093b7bf150ca253554688f1ce7eda448a23659e8
Rasmus <31828102+rasmuserlemann@users.noreply.github.com>
2020-10-30 13:27:23 +0200

    power

---
+++ Power_Study.R
@@ -0,0 +1,875 @@
+# NECESSARY R-PACKAGES
+
+library(VGAM)
+library(DiscreteWeibull)
+
+
+####################################################################
+
+# DISTRIBUTIONS, SIMULATION, ESTIMATION
+
+
+# BETA-GEOMETRIC DISTRIBUTION - shape1 and 2 are alpha and # beta in the paper
+
+
+# GEOMETRIC DISTRIBUTION
+
+
+
+# ESTIMATION IN BETA-GEOMETRIC AND GEOMETRIC - EXAMPLE
+
+# NOT RUN {
+# }
+
+
+# ESTIMATION IN DISCRETE WEIBULL TYPE I
+
+
+# zero=TRUE if 0 is a possible value, otherwise zero=FALSE # (default)
+
+
+
+###################################################################
+simcount = function(n,t)
+{
+  barsvec=randombars(n,t)
+  tell=c()
+  mm=max(barsvec)
+  for (k in 0:mm)
+  {
+    tell[k+1]=0
+    for (i in 1:n)
+    {
+      tell[k+1] = tell[k+1] + 1*(barsvec[i]==k)
+    }
+  }
+  return(tell)
+}
+
+# Draws from the bars and stars!
+#
+alg1 = function(n, t){
+  N = 0
+  V = 0
+  I = t+n-1
+  k = c()
+  while (N<n-1){
+    p = runif(1)
+    if (p<(n-1-N)/(t+n-1-N-V)){
+      k = c(I,k)
+      N = N+1
+      I = I-1
+    }
+    else{
+      V = V+1
+      I = I-1
+    }
+  }
+  return(k)
+}
+randombars = function(n,t){
+  s1 = alg1(n, t)
+  final = c(s1[1]-1)
+  for (ind in 2:length(s1)){
+    val = s1[ind]-ind-(s1[ind-1]-(ind-1))
+    final = append(final, val)
+  }
+  final = append(final, t-(s1[length(s1)]-(n-1)))
+  return (final)
+}
+
+ct=function(d)
+{
+  ct=c()
+  m = max(d)
+  for (i in 0:m) ct[i+1]=length(d[d==i])
+  return(ct) 
+}
+
+
+# Function that calculates r-th moment 
+# of the data 'count', wher k is the maximum value of x
+#
+mom <- function(k,count,r)
+{
+  vec=c(0:k)
+  momr = sum(vec^r*count)/sum(count)
+  return(momr)
+}
+
+
+# TEST STATISTICS
+
+# DATA SETS ARE USUALLY GIVEN BY VECTORS CALLED count
+
+# Calculation of n, t and p-hat are done inside the functions.
+
+# FUNCTIONS TO GO FROM count TO d AND VICE VERSA 
+
+# Function that takes 'count' to 'd'
+
+dt=function(count)
+{
+  m=length(count)-1
+  y=rep(0,count[1])
+  if (m > 0) 
+  {
+    for (i in 1:m) y=append(y,rep(i,count[i+1]))
+  }
+  return(y) 
+}
+
+
+# Function that takes 'count' to 'd'
+dt=function(count)
+{
+  m=length(count)-1
+  y=rep(0,count[1])
+  return(y) 
+}
+
+# Function that takes 'd' to 'count' 
+ct = function(dat){
+  out = c()
+  M = max(dat)
+  for (v in 0:M)
+    out = c(out, sum(dat == v))
+  out
+}
+
+# FUNCTION FOR THE VECTOR Z FROM THE ARTICLE (NOT USED LATER
+# SINCE IT IS INCORPORATED IN OTHER FUNCTIONS
+
+Z <- function(count)
+{
+  m=length(count)
+  n=sum(count) # number of obs
+  vec=c(0:(m-1))
+  t=sum(vec*count) #sum of xi
+  phat = n/(n+t)
+  O=cumsum(count)
+  E=c()
+  for (k in 1:m) E[k]=n*(1-(1-phat)^k)
+  Z=O-E
+  return(Z)
+}
+
+# THIS IS THE CRAMER VON MISES STATISTIC WHICH SUMS FROM 0 TO THE 
+# UPPER NUMBER MU GIVEN IN THE PAPER 
+
+CM <- function(count)
+{
+  m=length(count)
+  n=sum(count) # number of obs
+  vec=c(0:(m-1))
+  t=sum(vec*count) #sum of xi
+  phat = n/(n+t)
+  O=cumsum(count)
+  E=c()
+  ph=c()
+  for (k in 1:m) E[k]=n*(1-(1-phat)^k)
+  for (k in 1:m) ph[k]=phat*(1-phat)^(k-1)
+  Z=O-E
+  mm = m
+  while (ph[mm]*(1-phat) >= 0.001/n)
+  {
+    ph[mm+1] = ph[mm]*(1-phat)
+    Z[mm+1] = Z[mm]-n*ph[mm+1]
+    mm = mm+1
+  }
+  CM=sum(Z^2*ph)/n
+  return(CM)
+}
+
+
+# THIS IS THE ANDERSON-DARLING STATISTIC WHICH SUMS FROM 0 TO THE 
+# UPPER NUMBER MU GIVEN IN THE PAPER 
+
+AD <- function(count)
+{
+  m=length(count)
+  AD=0
+  if (m > 1)
+  {
+    n=sum(count) # number of obs
+    vec=c(0:(m-1))
+    t=sum(vec*count) #sum of xi
+    phat = n/(n+t)
+    O=cumsum(count)
+    E=c()
+    ph=c()
+    for (k in 1:m) E[k]=n*(1-(1-phat)^k)
+    for (k in 1:m) ph[k]=phat*(1-phat)^(k-1)
+    H=cumsum(ph)
+    Z=O-E
+    mm = m
+    while (ph[mm]*(1-phat) >= 0.001/n)
+    {
+      ph[mm+1] = ph[mm]*(1-phat)
+      Z[mm+1] = Z[mm]-n*ph[mm+1]
+      mm = mm+1
+    }
+    H=cumsum(ph)
+    AD=sum(Z^2*ph/(H*(1-H)))/n
+  }
+  return(AD)
+}
+
+
+# THIS IS THE KOLMOGOROV-SMIRNOV STATISTIC
+
+KS <- function(count)
+{
+  m=length(count)
+  n=sum(count) # number of obs
+  vec=c(0:(m-1))
+  t=sum(vec*count) #sum of xi
+  phat = n/(n+t)
+  O=cumsum(count)
+  E=c()
+  ph=c()
+  for (k in 1:m) E[k]=n*(1-(1-phat)^k)
+  for (k in 1:m) ph[k]=phat*(1-phat)^k
+  Z=O-E
+  maxz=0
+  for (r in 1:m)
+  {
+    if (abs(Z[r]) > maxz) maxz = abs(Z[r])
+  }
+  return(maxz)
+}
+
+
+# THIS IS THE Rao statistic (FULL LIKELIHOOD APPROACH)
+
+CR = function(count)
+{
+  d=dt(count)
+  n=length(d)
+  t = sum(d)
+  phat=n/(n+t)
+  sum1=0
+  sum2 = 0
+  for (i in 1:n)
+  {
+    if (d[i] != 0){
+    sum1=sum1+d[i]*log(d[i])
+    sum2 = sum2 + (d[i]+1)*log(d[i]+1)
+    }
+  }
+  sc=sum1-sum2
+  return(sc)
+}
+
+# THIS IS THE SCORE TEST FOR THE BETA-BINOMIAL 
+
+SB <- function(count)
+{
+  k=length(count)-1
+  m1=mom(k,count,1)
+  m2=mom(k,count,2)
+  m=length(count)
+  score = (m2-m1-2*m1^2)
+  return(score)
+}
+
+
+# THIS IS THE PREFERRED SCORE TEST FOR THE BETA-BINOMIAL.
+# IT IS CALLED SBabs, BUT CORRESPONDS TO THE SB0 IN THE PAPER
+# (SEE LAST LINE)
+
+SBabs <- function(count)
+{
+  k=length(count)-1
+  m1=mom(k,count,1)
+  m2=mom(k,count,2)
+  m=length(count)
+  score = (m2-m1-2*m1^2)
+  return(max(score,0))
+}
+
+
+# THIS IS THE MOMENT ESTIMATOR FOR THETA IN THE BETA-BINOMIAL
+# RECALL THAT THETA=0 IF THE DISGTRIBUTION IS GEOMETRIC
+
+theta <- function(count)
+{
+  k=length(count)-1
+  theta=0
+  if (k > 0)
+  {
+    m1=mom(k,count,1)
+    m2=mom(k,count,2)
+    theta=(m2-m1-2*m1^2)/(2*m2-m1^2+m1*m2)
+  }
+  return(theta)
+}
+
+
+# THIS IS The Weibull Score statistic - 
+# NOTE THE ABSOLUTE VALUE IN LAST LINE
+
+SW = function(count)
+{
+  d=dt(count)
+  n=length(d)
+  t = sum(d)
+  phat=n/(n+t)
+  sum1=0
+  sum2 = 0
+  for (i in 1:n)
+  {
+    if (d[i] != 0) sum1=sum1+d[i]*log(d[i])
+    sum2 = sum2 + (1-phat)*(d[i]+1)*log(d[i]+1)
+  }
+  sc=sum1-sum2
+  return(abs(sc))
+}
+
+# BELOW ARE THE ONE-SIDED TEST STATISTICS, 
+# SWL IS FOR ALTERNATIVE BETA < 1, SWU IS FOR BETA > 1
+
+SWL = function(count)
+{
+  d=dt(count)
+  n=length(d)
+  t = sum(d)
+  phat=n/(n+t)
+  sum1=0
+  sum2 = 0
+  for (i in 1:n)
+  {
+    if (d[i] != 0) sum1=sum1+d[i]*log(d[i])
+    sum2 = sum2 + (1-phat)*(d[i]+1)*log(d[i]+1)
+  }
+  sc=sum1-sum2
+  return(sc)
+}
+
+SWU = function(count)
+{
+  d=dt(count)
+  n=length(d)
+  t = sum(d)
+  phat=n/(n+t)
+  sum1=0
+  sum2 = 0
+  for (i in 1:n)
+  {
+    if (d[i] != 0) sum1=sum1+d[i]*log(d[i])
+    sum2 = sum2 + (1-phat)*(d[i]+1)*log(d[i]+1)
+  }
+  sc=sum1-sum2
+  return(-sc)
+}
+
+
+## THIS IS FOR P-VALUE CALCULATION FOR A GIVEN DATA VECTOR count
+## NOTE THE NUMBER OF SIMULATIONS, 'ant' THAT HAS TO BE SET (E.G. 10 000)
+## THE RESULT IS A VECTOR GIVING P-VALUES FOR THE 10 TEST STATISTICS 
+## IN ORDER
+
+pvalue = function(count,ant)
+{
+  m=length(count)
+  n=sum(count) # number of obs
+  vec=c(0:(m-1))
+  t=sum(vec*count) #sum of xi
+  W1=CM(count)
+  W2=AD(count)
+  W3=KS(count)
+  W4=CR(count)
+  W5=SB(count)
+  W6=SBabs(count)
+  W7=theta(count)
+  W8=SW(count)
+  W9=SWL(count)
+  W10=SWU(count)
+  tell1=0
+  tell2=0
+  tell3=0
+  tell4=0
+  tell5=0
+  tell6=0
+  tell7=0
+  tell8=0
+  tell9=0
+  tell10=0
+  for (i in 1:ant)
+  {
+    csim = simcount(n,t)
+    if (CM(csim) >= W1) tell1=tell1+1
+    if (AD(csim) >= W2) tell2=tell2+1
+    if (KS(csim) >= W3) tell3=tell3+1
+    if (CR(csim) >= W4) tell4=tell4+1
+    if (SB(csim) >= W5) tell5=tell5+1
+    if (SBabs(csim) >= W6) tell6=tell6+1
+    if (theta(csim) >= W7) tell7=tell7+1
+    if (SW(csim) >= W8) tell8=tell8+1
+    if (SWL(csim) >= W9) tell9=tell9+1
+    if (SWU(csim) >= W10) tell10=tell10+1
+  }
+  return(c(tell1,tell2,tell3,tell4,tell5,tell6,tell7,tell8,tell9,tell10)/ant)
+}
+
+
+## POWER SIMULATION FOR TESTS VERSUS BETA-GEOMETRIC.
+## HERE a=alpha, b=beta FROM ARTICLE. level=significance level,
+## number = number of samples from alternative distribution,
+## antsim = number of simulations to get p-value for each data set
+
+powerB = function(n,a,b,level,number,antsim)
+{
+  reject1=0
+  reject2=0
+  reject3=0
+  reject4=0
+  reject5=0
+  reject6=0
+  reject7=0
+  reject8=0
+  reject9=0
+  reject10=0
+  for (r in 1:number)
+  {
+    dat=rbetageom(n,a,b)
+    n=length(dat)
+    t=sum(dat)
+    count=ct(dat)
+    wobs1=CM(count)
+    wobs2=AD(count)
+    wobs3=KS(count)
+    wobs4=CR(count)
+    wobs5=SB(count)
+    wobs6=SBabs(count)
+    wobs7=theta(count)
+    wobs8=SW(count)
+    wobs9=SWL(count)
+    wobs10=SWU(count)
+    tell1=0
+    tell2=0
+    tell3=0
+    tell4=0
+    tell5=0
+    tell6=0
+    tell7=0
+    tell8=0
+    tell9=0
+    tell10=0
+    for (i in 1:antsim)
+    {
+      simd = simcount(n,t)
+      wnull1= CM(simd)
+      wnull2= AD(simd)
+      wnull3= KS(simd)
+      wnull4= CR(simd)
+      wnull5= SB(simd)
+      wnull6= SBabs(simd)
+      wnull7= theta(simd)
+      wnull8= SW(simd)
+      wnull9= SWL(simd)
+      wnull10= SWU(simd)
+      if (wobs1 <= wnull1) tell1=tell1+1
+      if (wobs2 <= wnull2) tell2=tell2+1
+      if (wobs3 <= wnull3) tell3=tell3+1
+      if (wobs4 <= wnull4) tell4=tell4+1
+      if (wobs5 <= wnull5) tell5=tell5+1
+      if (wobs6 <= wnull6) tell6=tell6+1
+      if (wobs7 <= wnull7) tell7=tell7+1
+      if (wobs8 <= wnull8) tell8=tell8+1
+      if (wobs9 <= wnull9) tell9=tell9+1
+      if (wobs10 <= wnull10) tell10=tell10+1
+    }
+    pval1 = tell1/antsim
+    pval2 = tell2/antsim
+    pval3 = tell3/antsim
+    pval4 = tell4/antsim
+    pval5 = tell5/antsim
+    pval6 = tell6/antsim
+    pval7 = tell7/antsim
+    pval8 = tell8/antsim
+    pval9 = tell9/antsim
+    pval10 = tell10/antsim
+    if (pval1 <= level) reject1=reject1+1
+    if (pval2 <= level) reject2=reject2+1
+    if (pval3 <= level) reject3=reject3+1
+    if (pval4 <= level) reject4=reject4+1
+    if (pval5 <= level) reject5=reject5+1
+    if (pval6 <= level) reject6=reject6+1
+    if (pval7 <= level) reject7=reject7+1
+    if (pval8 <= level) reject8=reject8+1
+    if (pval9 <= level) reject9=reject9+1
+    if (pval10 <= level) reject10=reject10+1
+  }
+  power1=reject1/number
+  power2=reject2/number
+  power3=reject3/number
+  power4=reject4/number
+  power5=reject5/number
+  power6=reject6/number
+  power7=reject7/number
+  power8=reject8/number
+  power9=reject9/number
+  power10=reject10/number
+  return(c(power1,power2,power3,power4,power5,power6,power7,power8,power9,power10))
+}
+
+
+
+## POWER SIMULATION FOR TESTS VERSUS DISCRETE WEIBULL TYPE I.
+## HERE q and beta ARE AS IN ARTICLE. level=significance level,
+## number = number of samples from alternative distribution,
+## antsim = number of simulations to get p-value for each data set
+
+powerW = function(n,q,beta,level,number,antsim)
+{
+  reject1=0
+  reject2=0
+  reject3=0
+  reject4=0
+  reject5=0
+  reject6=0
+  reject7=0
+  reject8=0
+  reject9=0
+  reject10=0
+  for (r in 1:number)
+  {
+    dat=rdweibull(n,q,beta,zero=TRUE)
+    n=length(dat)
+    t=sum(dat)
+    count=ct(dat)
+    wobs1=CM(count)
+    wobs2=AD(count)
+    wobs3=KS(count)
+    wobs4=CR(count)
+    wobs5=SB(count)
+    wobs6=SBabs(count)
+    wobs7=theta(count)
+    wobs8=SW(count)
+    wobs9=SWL(count)
+    wobs10=SWU(count)
+    tell1=0
+    tell2=0
+    tell3=0
+    tell4=0
+    tell5=0
+    tell6=0
+    tell7=0
+    tell8=0
+    tell9=0
+    tell10=0
+    for (i in 1:antsim)
+    {
+      simd = simcount(n,t)
+      wnull1= CM(simd)
+      wnull2= AD(simd)
+      wnull3= KS(simd)
+      wnull4= CR(simd)
+      wnull5= SB(simd)
+      wnull6= SBabs(simd)
+      wnull7= theta(simd)
+      wnull8= SW(simd)
+      wnull9= SWL(simd)
+      wnull10= SWU(simd)
+      if (wobs1 <= wnull1) tell1=tell1+1
+      if (wobs2 <= wnull2) tell2=tell2+1
+      if (wobs3 <= wnull3) tell3=tell3+1
+      if (wobs4 <= wnull4) tell4=tell4+1
+      if (wobs5 <= wnull5) tell5=tell5+1
+      if (wobs6 <= wnull6) tell6=tell6+1
+      if (wobs7 <= wnull7) tell7=tell7+1
+      if (wobs8 <= wnull8) tell8=tell8+1
+      if (wobs9 <= wnull9) tell9=tell9+1
+      if (wobs10 <= wnull10) tell10=tell10+1
+    }
+    pval1 = tell1/antsim
+    pval2 = tell2/antsim
+    pval3 = tell3/antsim
+    pval4 = tell4/antsim
+    pval5 = tell5/antsim
+    pval6 = tell6/antsim
+    pval7 = tell7/antsim
+    pval8 = tell8/antsim
+    pval9 = tell9/antsim
+    pval10 = tell10/antsim
+    if (pval1 <= level) reject1=reject1+1
+    if (pval2 <= level) reject2=reject2+1
+    if (pval3 <= level) reject3=reject3+1
+    if (pval4 <= level) reject4=reject4+1
+    if (pval5 <= level) reject5=reject5+1
+    if (pval6 <= level) reject6=reject6+1
+    if (pval7 <= level) reject7=reject7+1
+    if (pval8 <= level) reject8=reject8+1
+    if (pval9 <= level) reject9=reject9+1
+    if (pval10 <= level) reject10=reject10+1
+  }
+  power1=reject1/number
+  power2=reject2/number
+  power3=reject3/number
+  power4=reject4/number
+  power5=reject5/number
+  power6=reject6/number
+  power7=reject7/number
+  power8=reject8/number
+  power9=reject9/number
+  power10=reject10/number
+  return(c(power1,power2,power3,power4,power5,power6,power7,power8,power9,power10))
+}
+
+
+## POWER SIMULATION FOR TESTS UNDER NULL HYPOTHESIS
+## HERE p IS THE PROBABILITY PARAMETER, level=significance level,
+## number = number of initial samples,
+## antsim = number of simulations to get p-value for each data set
+
+powerG = function(n,p,level,number,antsim)
+{
+  reject1=0
+  reject2=0
+  reject3=0
+  reject4=0
+  reject5=0
+  reject6=0
+  reject7=0
+  reject8=0
+  reject9=0
+  reject10=0
+  for (r in 1:number)
+  {
+    dat=rgeom(n,p)
+    n=length(dat)
+    t=sum(dat)
+    count=ct(dat)
+    wobs1=CM(count)
+    wobs2=AD(count)
+    wobs3=KS(count)
+    wobs4=CR(count)
+    wobs5=SB(count)
+    wobs6=SBabs(count)
+    wobs7=theta(count)
+    wobs8=SW(count)
+    wobs9=SWL(count)
+    wobs10=SWU(count)
+    tell1=0
+    tell2=0
+    tell3=0
+    tell4=0
+    tell5=0
+    tell6=0
+    tell7=0
+    tell8=0
+    tell9=0
+    tell10=0
+    for (i in 1:antsim)
+    {
+      simd = simcount(n,t)
+      wnull1= CM(simd)
+      wnull2= AD(simd)
+      wnull3= KS(simd)
+      wnull4= CR(simd)
+      wnull5= SB(simd)
+      wnull6= SBabs(simd)
+      wnull7= theta(simd)
+      wnull8= SW(simd)
+      wnull9= SWL(simd)
+      wnull10= SWU(simd)
+      if (wobs1 <= wnull1) tell1=tell1+1
+      if (wobs2 <= wnull2) tell2=tell2+1
+      if (wobs3 <= wnull3) tell3=tell3+1
+      if (wobs4 <= wnull4) tell4=tell4+1
+      if (wobs5 <= wnull5) tell5=tell5+1
+      if (wobs6 <= wnull6) tell6=tell6+1
+      if (wobs7 <= wnull7) tell7=tell7+1
+      if (wobs8 <= wnull8) tell8=tell8+1
+      if (wobs9 <= wnull9) tell9=tell9+1
+      if (wobs10 <= wnull10) tell10=tell10+1
+    }
+    pval1 = tell1/antsim
+    pval2 = tell2/antsim
+    pval3 = tell3/antsim
+    pval4 = tell4/antsim
+    pval5 = tell5/antsim
+    pval6 = tell6/antsim
+    pval7 = tell7/antsim
+    pval8 = tell8/antsim
+    pval9 = tell9/antsim
+    pval10 = tell10/antsim
+    if (pval1 <= level) reject1=reject1+1
+    if (pval2 <= level) reject2=reject2+1
+    if (pval3 <= level) reject3=reject3+1
+    if (pval4 <= level) reject4=reject4+1
+    if (pval5 <= level) reject5=reject5+1
+    if (pval6 <= level) reject6=reject6+1
+    if (pval7 <= level) reject7=reject7+1
+    if (pval8 <= level) reject8=reject8+1
+    if (pval9 <= level) reject9=reject9+1
+    if (pval10 <= level) reject10=reject10+1
+  }
+  power1=reject1/number
+  power2=reject2/number
+  power3=reject3/number
+  power4=reject4/number
+  power5=reject5/number
+  power6=reject6/number
+  power7=reject7/number
+  power8=reject8/number
+  power9=reject9/number
+  power10=reject10/number
+  return(c(power1,power2,power3,power4,power5,power6,power7,power8,power9,power10))
+}
+
+# EXAMPLE TO SHOW THAT THE SIZE (TYPE I ERROR PROBABILITIES CAN BE MUCH 
+# LESS THAN =0.05 IF n IS SMALL, AND APPROX EQUAL TO 0.05 IF N IS LARGE.
+# NOTE THAT THE RESULTS 0.057 > 0.05 ARE DUE TO MONTE CARLO ERROR. THEORETICALLY
+# THEY ARE < 0.05 (BUT CLOSE IF n IS LARGE)
+
+simcount = function(n,t)
+{
+  barsvec=randombars(n,t)
+  tell=c()
+  mm=max(barsvec)
+  for (k in 0:mm)
+  {
+    tell[k+1]=0
+    for (i in 1:n)
+    {
+      tell[k+1] = tell[k+1] + 1*(barsvec[i]==k)
+    }
+  }
+  return(tell)
+}
+
+# Draws from the stars and bars!
+#
+alg1 = function(n, t){
+  N = 0
+  V = 0
+  I = t+n-1
+  k = c()
+  while (N<n-1){
+    p = runif(1)
+    if (p<(n-1-N)/(t+n-1-N-V)){
+      k = c(I,k)
+      N = N+1
+      I = I-1
+    }
+    else{
+      V = V+1
+      I = I-1
+    }
+  }
+  return(k)
+}
+randombars = function(n,t){
+  s1 = alg1(n, t)
+  final = c(s1[1]-1)
+  for (ind in 2:length(s1)){
+    val = s1[ind]-ind-(s1[ind-1]-(ind-1))
+    final = append(final, val)
+  }
+  final = append(final, t-(s1[length(s1)]-(n-1)))
+  return (final)
+}
+
+# Function that takes 'd' to 'count' 
+ct = function(dat){
+  out = c()
+  M = max(dat)
+  for (v in 0:M)
+    out = c(out, sum(dat == v))
+  out
+}
+
+#Cramer-von Mises test
+CM <- function(count)
+{
+  m=length(count)
+  n=sum(count) # number of obs
+  vec=c(0:(m-1))
+  t=sum(vec*count) #sum of xi
+  phat = n/(n+t)
+  O=cumsum(count)
+  E=c()
+  ph=c()
+  for (k in 1:m) E[k]=n*(1-(1-phat)^k)
+  for (k in 1:m) ph[k]=phat*(1-phat)^(k-1)
+  Z=O-E
+  mm = m
+  while (ph[mm]*(1-phat) >= 0.001/n)
+  {
+    ph[mm+1] = ph[mm]*(1-phat)
+    Z[mm+1] = Z[mm]-n*ph[mm+1]
+    mm = mm+1
+  }
+  CM=sum(Z^2*ph)/n
+  return(CM)
+}
+
+n = 100
+d1 = rgeom(n, 0.5)
+d2 = rdweibull(n,0.7,0.8,zero=TRUE)
+datacount1 = ct(d1)
+datacount2 = ct(d2)
+#Critical values
+cramer1 = CM(datacount1)
+cramer2 = CM(datacount2)
+
+#Histograms
+hist(d1, xlab="", ylab = "", main = "")
+hist(d2, xlab="", ylab = "", main = "")
+
+#Sufficient statistic values
+t1 = sum(d1)
+t2 = sum(d2)
+
+#MLE estimates under the null hypothesis
+mle1 = n/(t1+n)
+mle2 = n/(t2+n)
+
+#Cramer-von Mises test distribution via parametric bootstrapping
+test1 = c()
+test2 = c()
+L = 10**(5)
+for (k in 1:L){
+  dp1 = rgeom(n, mle1)
+  dp2 = rgeom(n, mle2)
+  c1 = ct(dp1)
+  c2 = ct(dp2)
+  test1 = c(test1, CM(c1))
+  test2 = c(test2, CM(c2))
+}
+plot(density(test1), ylim = c(0,11.7), xlim = c(0,0.5), xlab="", ylab = "", main = "")
+plot(density(test2), ylim = c(0,9.1), xlim = c(0,0.9), xlab="", ylab = "", main = "")
+
+#Conditional test distributions
+condtest1 = c()
+condtest2 = c()
+for (k in 1:L){
+  condc1 = simcount(n, t1)
+  condc2 = simcount(n, t2)
+  condtest1 = c(condtest1, CM(condc1))
+  condtest2 = c(condtest2, CM(condc2))
+}
+par(new=TRUE)
+plot(density(condtest1), ylim = c(0,11.7), xlim = c(0,0.5), xlab="", ylab = "", main = "", col = "blue")
+plot(density(condtest2), ylim = c(0,9.1), xlim = c(0,0.9), xlab="", ylab = "", main = "", col = "blue")
+abline(v=cramer1, col="green")
+abline(v=cramer2, col="green")
+
+#Quantiles
+print(sort(test1)[as.integer(L*0.95)])
+print(sort(test2)[as.integer(L*0.95)])
+print(sort(condtest1)[as.integer(L*0.95)])
+print(sort(condtest2)[as.integer(L*0.95)])
+
+d = c(rnorm(30, 0, 1), rnorm(70,0,5))
+plot(d, type="l", ylab="")
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Power study code in the paper *Conditional Goodness-of-Fit Tests for Discrete Distributions*
-The code is written in R.
+The code is written in R. It was written in collaboration with Prof. Bo Henry Lindqvist.
 
 ## Anderson-Darling, Cramer-von Mises and Kolmogorov Smirnov test statistics
-These are regular quadratic goodness-of-fit test statistics. Null hypothesis model maximum likelihood estimates are used. For the geometric distribution, this is calculated by n/(sum(d)+n).
+Test statistics functions are AD, CM, KS. The first two are regular quadratic goodness-of-fit test statistics and the last one is a maximal type test. Only null hypothesis model maximum likelihood estimates are used. For the geometric distribution, this is calculated by n/(sum(d)+n).
+
+## Likelihood based tests
+Test statistics functions are CR, SB, SBabs, theta, SW, SWL, SWU. The first one is the so-called full likelihood test. Its main advantage is in detecting deviations from homogeneity in the sample. The next 3 tests use the beta-geometric distribution as the alternative in the likelihood ratio test. It detects deviations towards the beta-geometric distribution. Last 3 tests are also likelihood ratio tests with the Discrete Weibull type I as the alternative distribution.
