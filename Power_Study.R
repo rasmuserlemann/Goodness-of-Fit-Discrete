aa144a5fbb418d2e8397f28575730fa51fca9f4f
Rasmus <31828102+rasmuserlemann@users.noreply.github.com>
2020-10-30 13:40:21 +0200

    power

--- Power_Study.R
+++ Power_Study.R
@@ -3,33 +3,10 @@
 library(VGAM)
 library(DiscreteWeibull)
 
-
-####################################################################
-
-# DISTRIBUTIONS, SIMULATION, ESTIMATION
-
-
-# BETA-GEOMETRIC DISTRIBUTION - shape1 and 2 are alpha and # beta in the paper
-
-
-# GEOMETRIC DISTRIBUTION
-
-
-
-# ESTIMATION IN BETA-GEOMETRIC AND GEOMETRIC - EXAMPLE
-
-# NOT RUN {
-# }
-
-
-# ESTIMATION IN DISCRETE WEIBULL TYPE I
-
-
-# zero=TRUE if 0 is a possible value, otherwise zero=FALSE # (default)
-
-
-
 ###################################################################
+
+#Drawing conditional simulations
+
 simcount = function(n,t)
 {
   barsvec=randombars(n,t)
@@ -67,6 +44,7 @@ alg1 = function(n, t){
   }
   return(k)
 }
+
 randombars = function(n,t){
   s1 = alg1(n, t)
   final = c(s1[1]-1)
@@ -89,7 +67,7 @@ ct=function(d)
 
 # Function that calculates r-th moment 
 # of the data 'count', wher k is the maximum value of x
-#
+
 mom <- function(k,count,r)
 {
   vec=c(0:k)
@@ -261,7 +239,7 @@ CR = function(count)
   return(sc)
 }
 
-# THIS IS THE SCORE TEST FOR THE BETA-BINOMIAL 
+# THIS IS THE SCORE TEST FOR THE BETA-GEOMETRIC
 
 SB <- function(count)
 {
@@ -274,7 +252,7 @@ SB <- function(count)
 }
 
 
-# THIS IS THE PREFERRED SCORE TEST FOR THE BETA-BINOMIAL.
+# THIS IS THE PREFERRED SCORE TEST FOR THE BETA-GEOMETRIC.
 # IT IS CALLED SBabs, BUT CORRESPONDS TO THE SB0 IN THE PAPER
 # (SEE LAST LINE)
 
@@ -289,7 +267,7 @@ SBabs <- function(count)
 }
 
 
-# THIS IS THE MOMENT ESTIMATOR FOR THETA IN THE BETA-BINOMIAL
+# THIS IS THE MOMENT ESTIMATOR FOR THETA IN THE BETA-GEOMETRIC
 # RECALL THAT THETA=0 IF THE DISGTRIBUTION IS GEOMETRIC
 
 theta <- function(count)
@@ -724,152 +702,5 @@ powerG = function(n,p,level,number,antsim)
 # NOTE THAT THE RESULTS 0.057 > 0.05 ARE DUE TO MONTE CARLO ERROR. THEORETICALLY
 # THEY ARE < 0.05 (BUT CLOSE IF n IS LARGE)
 
-simcount = function(n,t)
-{
-  barsvec=randombars(n,t)
-  tell=c()
-  mm=max(barsvec)
-  for (k in 0:mm)
-  {
-    tell[k+1]=0
-    for (i in 1:n)
-    {
-      tell[k+1] = tell[k+1] + 1*(barsvec[i]==k)
-    }
-  }
-  return(tell)
-}
-
-# Draws from the stars and bars!
-#
-alg1 = function(n, t){
-  N = 0
-  V = 0
-  I = t+n-1
-  k = c()
-  while (N<n-1){
-    p = runif(1)
-    if (p<(n-1-N)/(t+n-1-N-V)){
-      k = c(I,k)
-      N = N+1
-      I = I-1
-    }
-    else{
-      V = V+1
-      I = I-1
-    }
-  }
-  return(k)
-}
-randombars = function(n,t){
-  s1 = alg1(n, t)
-  final = c(s1[1]-1)
-  for (ind in 2:length(s1)){
-    val = s1[ind]-ind-(s1[ind-1]-(ind-1))
-    final = append(final, val)
-  }
-  final = append(final, t-(s1[length(s1)]-(n-1)))
-  return (final)
-}
-
-# Function that takes 'd' to 'count' 
-ct = function(dat){
-  out = c()
-  M = max(dat)
-  for (v in 0:M)
-    out = c(out, sum(dat == v))
-  out
-}
-
-#Cramer-von Mises test
-CM <- function(count)
-{
-  m=length(count)
-  n=sum(count) # number of obs
-  vec=c(0:(m-1))
-  t=sum(vec*count) #sum of xi
-  phat = n/(n+t)
-  O=cumsum(count)
-  E=c()
-  ph=c()
-  for (k in 1:m) E[k]=n*(1-(1-phat)^k)
-  for (k in 1:m) ph[k]=phat*(1-phat)^(k-1)
-  Z=O-E
-  mm = m
-  while (ph[mm]*(1-phat) >= 0.001/n)
-  {
-    ph[mm+1] = ph[mm]*(1-phat)
-    Z[mm+1] = Z[mm]-n*ph[mm+1]
-    mm = mm+1
-  }
-  CM=sum(Z^2*ph)/n
-  return(CM)
-}
-
-n = 100
-d1 = rgeom(n, 0.5)
-d2 = rdweibull(n,0.7,0.8,zero=TRUE)
-datacount1 = ct(d1)
-datacount2 = ct(d2)
-#Critical values
-cramer1 = CM(datacount1)
-cramer2 = CM(datacount2)
-
-#Histograms
-hist(d1, xlab="", ylab = "", main = "")
-hist(d2, xlab="", ylab = "", main = "")
-
-#Sufficient statistic values
-t1 = sum(d1)
-t2 = sum(d2)
-
-#MLE estimates under the null hypothesis
-mle1 = n/(t1+n)
-mle2 = n/(t2+n)
-
-#Cramer-von Mises test distribution via parametric bootstrapping
-test1 = c()
-test2 = c()
-L = 10**(5)
-for (k in 1:L){
-  dp1 = rgeom(n, mle1)
-  dp2 = rgeom(n, mle2)
-  c1 = ct(dp1)
-  c2 = ct(dp2)
-  test1 = c(test1, CM(c1))
-  test2 = c(test2, CM(c2))
-}
-plot(density(test1), ylim = c(0,11.7), xlim = c(0,0.5), xlab="", ylab = "", main = "")
-plot(density(test2), ylim = c(0,9.1), xlim = c(0,0.9), xlab="", ylab = "", main = "")
-
-#Conditional test distributions
-condtest1 = c()
-condtest2 = c()
-for (k in 1:L){
-  condc1 = simcount(n, t1)
-  condc2 = simcount(n, t2)
-  condtest1 = c(condtest1, CM(condc1))
-  condtest2 = c(condtest2, CM(condc2))
-}
-par(new=TRUE)
-plot(density(condtest1), ylim = c(0,11.7), xlim = c(0,0.5), xlab="", ylab = "", main = "", col = "blue")
-plot(density(condtest2), ylim = c(0,9.1), xlim = c(0,0.9), xlab="", ylab = "", main = "", col = "blue")
-abline(v=cramer1, col="green")
-abline(v=cramer2, col="green")
-
-#Quantiles
-print(sort(test1)[as.integer(L*0.95)])
-print(sort(test2)[as.integer(L*0.95)])
-print(sort(condtest1)[as.integer(L*0.95)])
-print(sort(condtest2)[as.integer(L*0.95)])
-
-d = c(rnorm(30, 0, 1), rnorm(70,0,5))
-plot(d, type="l", ylab="")
-
-
-
-
-
-
-
-
+#Example
+powerG(25,0.5,0.1,100,100)
--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 The code is written in R. It was written in collaboration with Prof. Bo Henry Lindqvist.
 
 ## Anderson-Darling, Cramer-von Mises and Kolmogorov Smirnov test statistics
-Test statistics functions are AD, CM, KS. The first two are regular quadratic goodness-of-fit test statistics and the last one is a maximal type test. Only null hypothesis model maximum likelihood estimates are used. For the geometric distribution, this is calculated by n/(sum(d)+n).
+Test statistics functions are AD, CM, KS. The first two are regular quadratic goodness-of-fit test statistics and the last one is a maximal type test. Only the null hypothesis model maximum likelihood estimates are used. For the geometric distribution, this is calculated by n/(sum(d)+n).
 
 ## Likelihood based tests
-Test statistics functions are CR, SB, SBabs, theta, SW, SWL, SWU. The first one is the so-called full likelihood test. Its main advantage is in detecting deviations from homogeneity in the sample. The next 3 tests use the beta-geometric distribution as the alternative in the likelihood ratio test. It detects deviations towards the beta-geometric distribution. Last 3 tests are also likelihood ratio tests with the Discrete Weibull type I as the alternative distribution.
+Test statistics functions are CR, SB, SBabs, theta, SW, SWL, SWU. The first one is the so-called full likelihood test. Its main advantage is in detecting deviations from homogeneity in the sample. The next 3 tests use the beta-geometric distribution as the alternative in the likelihood ratio test. It detects deviations towards the beta-geometric distribution. Last 3 tests are also likelihood ratio tests with the Discrete Weibull type I as the alternative distribution. Likelihood based tests use MoM instead.
+
+##Setup
+To run the power simulations, choose the alternative. Each alternative that was studied in the paper, has a separate function for it. For example, if the sample comes from the Beta-Geometric distribution, use the function 
+```
+powerB
+```
+Parameters in the alternative distribution can be changed inside the function body. To continue our example, for the Beta-Geometric case, we would change 
+```
+rbetageom(n,a,b)
+```
